48a9db195154cc1d24ca11eb9b36bf62ac372bca
christian <christian@somewhere.earth>
2024-07-07 10:07:13 +0000

    progress

--- gather_records.py
+++ gather_records.py
@@ -23,11 +23,10 @@ headers = {
 domain_name = domain_names[0]
 
 def run_samba_tool(cmd, *args):
-    args = ''.join(args) # Convert list of arguments to a space separated string for samba-tool ingestion
+    args = ' '.join(args) # Convert list of arguments to a space separated string for samba-tool ingestion
     try:
         logger.info(f'Running: {samba_tool} dns {cmd} {domain_controller} {args}')
         # Run samba-tool, redirect stderr to stdout, decode bytes in utf-8 to str
-        print(str(args))
         raw_result = subprocess.check_output([samba_tool, 'dns', cmd, domain_controller, args], stderr=subprocess.STDOUT).decode() 
         # Split output into array of lines
         lines = raw_result.splitlines()
@@ -48,7 +47,7 @@ def run_samba_tool(cmd, *args):
             return(raw_result)
     except subprocess.CalledProcessError as err:
         # ^ This is a catch-all for a non-zero return code
-        logger.info(f'Error running {cmd} with argumenti(s) {args}')
+        logger.info(f'Error running dns {cmd} with argument(s) {args}:\n{err.output.decode()}')
         # Look at output for samba-tool specific error code (decode bytes to utf-8)
         if 'WERR_DNS_ERROR_ZONE_DOES_NOT_EXIST' in err.output.decode():
             return False, err.output.decode()
@@ -80,19 +79,24 @@ if response.status_code == 200:
     zones = response.json().get('result')
     if zones:
         zone_id = zones[0]['id']
-        print(f'Zone ID for {domain_name}: {zone_id}')
+        logger.debug(f'Zone ID for {domain_name}: {zone_id}')
 
+        # Now that we have the Zone ID, use it to get records for that zone
         # Cloudflare API endpoint to list DNS records
         dns_records_endpoint = f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records'
 
         # Get DNS records for the zone
-        response = requests.get(dns_records_endpoint, headers=headers)
+        response = requests.get(dns_records_endpoint, headers=headers) #.get default value None
         if response.status_code == 200:
+            logger.info(f'Public DNS records for {domain_name} retrieved!')
             dns_records = response.json().get('result')
-            for record in dns_records:
-                print(f"Type: {record['type']}, Name: {record['name']}, Content: {record['content']}")
+            #for record in dns_records:
+                #print(f"Type: {record['type']}, Name: {record['name']}, Content: {record['content']}")
+                #run_samba_tool('query', domain_name, record['name'], record['type'])
+            print(run_samba_tool('query', 'smvirtual.biz', 'dummy', 'A'))
         else:
             logger.error('Failed to retrieve DNS records:', response.json())
+
     else:
         logger.warning(f'No zones found for domain: {domain_name}')
 else:
